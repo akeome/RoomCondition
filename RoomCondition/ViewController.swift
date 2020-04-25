74fc3cbb068b73442f06f2cc6ae90ed0149988ae
akeome <akeomestudio@gmail.com>
2020-04-26 01:13:15 +0900

    first commit

---
+++ RoomCondition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RoomCondition
+//
+//  Created by 山下優樹 on 2020/04/26.
+//  Copyright © 2020 Yuki Yamashita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RoomConditionTodayExtension/TodayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodayViewController.swift
+//  RoomConditionTodayExtension
+//
+//  Created by 山下優樹 on 2020/04/26.
+//  Copyright © 2020 Yuki Yamashita. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
