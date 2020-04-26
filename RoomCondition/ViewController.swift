e510224e7280841badfd7113e34c2fbdd4398853
akeome <akeomestudio@gmail.com>
2020-04-26 23:01:48 +0900

    本体アプリでダッシュボートを表示するためのSFSafariViewControllerを追加

--- RoomCondition/ViewController.swift
+++ RoomCondition/ViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let url = URL(string: "http://192.168.100.50:1201/dashboard")
+        if let url = url {
+            let safari = SFSafariViewController(url: url)
+            present(safari, animated: false)
+        }
     }
-
-
 }
